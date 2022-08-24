4687b64107d89cc49e3337e1d21c82e0aa11616e
Matthias Putz <matthias@simpl-rocks.com>
2022-08-24 15:44:23 +0200

    Added flake.nix and some tools

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Spark SQS Receiver";
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+      nixpkgsFor = system: import nixpkgs { inherit system; };
+    in
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgsFor system;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            awscli2
+            jdk11
+            just
+            maven
+            scala
+          ];
+          shellHook = ''
+          '';
+        };
+      });
+}
+
